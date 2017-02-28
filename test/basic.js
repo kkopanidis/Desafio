85a2ba7caa8a0c2c265060b13585ced21a76d5be
NightmareNyx <nightmare7777777@gmail.com>
2017-02-28 21:38:32 +0200

    testing

--- test/basic.js
+++ test/basic.js
@@ -6,13 +6,81 @@ var chaiHttp = require('chai-http');
 var should = chai.should();
 
 chai.use(chaiHttp);
-//TODO change the code below with what we want to test
-//Obviously is copied from fably
 
-describe('stories', function () {
-    it('should get the first 10 stories on /stories GET', function (done) {
+var userid = 0; //logged in user
+var Cookies;
+
+describe('Register', function () {
+    it('should register a new user', function (done) {
         chai.request(server)
-            .get('/stories')
+            .post('/register')
+            .send({
+                "username": "utest",
+                "password": "test123",
+                "email": "utest@test.com",
+                "dob": "1995-11-25"
+            })
+            .expect(200)
+            .end(function (err, res) {
+                res.body.username.should.equal('utest');
+                res.body.email.should.equal('utest@test.com');
+                res.body.password.should.equal('test123');
+                res.body.dob.should.equal('1995-11-25');
+                done();
+            });
+    });
+});
+
+describe('Login and sessions', function () {
+    it('should create user session for valid user', function (done) {
+        chai.request(server)
+            .post('/api/oauth/token')
+            .set('Accept', 'application/json')
+            .send({
+                "grant_type": "password",
+                "client_id": "Axxh45u4bdajGDshjk21n",
+                "client_secret": "d13e~223~!!@$5dasd",
+                "username": "utest@test.com",
+                "password": "test123"
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                res.body.id.should.equal('1');
+                res.body.username.should.equal('utest@test.com');
+                res.body.password.should.equal('test123');
+                res.body.cliend_id.should.equal("Axxh45u4bdajGDshjk21n");
+                res.body.client_secret.should.equal("d13e~223~!!@$5dasd");
+                res.body.grant_type.should.equal("password");
+                // Save the cookie to use it later to retrieve the session
+                Cookies = res.body.access_token;
+                userid = res.body.id; // pws pairnw to active user id?
+                done();
+            });
+    });
+    it('should get user session for current user', function (done) {
+        var req = request(app).get('/api/oauth/token');
+        // Set cookie to get saved user session
+        req.cookies = Cookies;
+        req.set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                res.body.id.should.equal('1');
+                res.body.username.should.equal('utest@test.com');
+                res.body.password.should.equal('test123');
+                res.body.cliend_id.should.equal("Axxh45u4bdajGDshjk21n");
+                res.body.client_secret.should.equal("d13e~223~!!@$5dasd");
+                res.body.grant_type.should.equal("password");
+                done();
+            });
+    });
+});
+
+describe('challenge flow', function () {
+    it('should get ALL challenges on /flow GET', function (done) {
+        chai.request(server)
+            .get('/flow')
             .end(function (err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
@@ -22,38 +90,40 @@ describe('stories', function () {
                 done();
             });
     });
-    it('should get specific story /stories/:id GET', function (done) {
+    it('should create a new challenge on / POST', function (done) {
         chai.request(server)
-            .get('/stories/5816b687bccdb29c24f1316d')
+            .post('/')
+            .send({'title': 'testaki', 'desc': 'perigrafh lol', 'type': 'food', 'issuer': userid})
             .end(function (err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('object');
-                res.body.should.have.property('_id');
+                res.body.should.have.property('SUCCESS');
+                res.body.SUCCESS.should.be.a('object');
+                res.body.SUCCESS.should.have.property('title');
+                res.body.SUCCESS.should.have.property('desc');
+                res.body.SUCCESS.should.have.property('type');
+                res.body.SUCCESS.should.have.property('issuer');
+                res.body.SUCCESS.should.have.property('_id');
+                res.body.SUCCESS.title.should.equal('testaki');
+                res.body.SUCCESS.desc.should.equal('perigrafh lol');
+                res.body.SUCCESS.type.should.equal('food');
+                res.body.SUCCESS.issuer.should.equal(userid);
                 done();
             });
     });
-    it('should get full content on a specific story /stories/full/:id GET', function (done) {
+    // TODO
+    it('should create a new gauntlent on /gaunlet POST', function (done) {
         chai.request(server)
-            .get('/stories/full/5816b687bccdb29c24f1316d')
+            .post('/gaunlet')
+            .send({})
             .end(function (err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('object');
-                res.body.should.have.property('content');
-                res.body.content.should.be.a('object');
-                done();
-            });
-    });
-    it('should get content on a specific story /stories/content/:id GET', function (done) {
-        chai.request(server)
-            .get('/stories/content/5816b687bccdb29c24f1316d')
-            .end(function (err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body.should.have.property('images');
-                res.body.images.should.be.a('array');
+                res.body.should.have.property('SUCCESS');
+                res.body.SUCCESS.should.be.a('object');
+
                 done();
             });
     });
